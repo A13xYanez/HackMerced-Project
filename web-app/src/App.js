845bea2226b61f61d77707729ca14f245157962a
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-03-09 08:22:34 -0800

    Created Header, Main, and Footer components

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './App.css';
 import Nav from './Components/Nav'
+import Header from './Components/Header'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
 
 export default function App() {
   return (
     <>
       <Nav />
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
