c2877815e8da8c819b86b69031e83d3cdbaafd67
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-03-09 09:07:30 -0800

    Created a title and CSS for title in Main component

--- web-app/src/Components/Main.js
+++ web-app/src/Components/Main.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Main() {
     return (
-        <main>
-            
+        <main className='main'>
+            <section className='main--title'>
+                <h1>Details</h1>
+                <a href='./'><button>Learn More</button></a>
+            </section>
         </main>
     )
 }
