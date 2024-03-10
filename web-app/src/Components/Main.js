5b5523ee6bac214db3e8fb8d10f0a660e54a884d
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-03-09 17:03:36 -0800

    Removed effects due to issues being caused

--- web-app/src/Components/Main.js
+++ web-app/src/Components/Main.js
@@ -1,21 +1,6 @@
 import React from 'react'
 
 export default function Main() {
-    // Animates the details cards when they become visible
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show')
-            } else {
-                entry.target.classList.remove('show')
-            }
-        });
-    });
-    
-    const hiddenElements = document.querySelectorAll('.details--hidden');
-    hiddenElements.forEach((el) => observer.observe(el));
-    
-    
     return (
         <main className='main'>
             <section className='main--title'>
@@ -27,7 +12,7 @@ export default function Main() {
                     <div>
                         <h2>Polyculture</h2>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Polyculture planting is the practice of simultaneously growing different crops at once in the same area, promoting biodiversity and ecological balance. By practicing polyculture and pairing complementary plants, they won’t have to battle for the same resources and nutrients and mimic natural vegetation to help the soil be more sustainable. Our simulation demonstrates the benefits of polyculture, its role in improving soil health, reducing pests, and increasing crop yield over time. 
                         </p>
                     </div>
                     <img src={require('../Images/polyculture.jpg')} />
@@ -37,7 +22,7 @@ export default function Main() {
                     <div>
                         <h2>Monoculture</h2>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Monoculture involves cultivating a single crop species at a time in a specific area, a method often used for its short-term efficiency and economic benefits. This lets farmers specialize and focus on one crop at a time, understand what it needs to thrive and not worry about different planting schedules and harvest times. However, this practice can lead to soil degradation, increased vulnerability to pests, and a reliance on chemical fertilizers and pesticides. Our simulation demonstrates the challenges and impacts of monoculture farming, and its effects on the environment and agricultural sustainability.
                         </p>
                     </div>
                 </article>
