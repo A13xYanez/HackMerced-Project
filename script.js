85cce1bc80402ae97deb61b47ce8d18444ad1150
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-03-09 23:12:39 -0800

    Created simulation section

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.details--hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+// Game control side bar
+const sideBarBox = document.querySelector("#controls");
+let isOpen = false;
+function openSideBar() {
+    if (isOpen == false) {
+        sideBarBox.classList.remove('closeBar');
+        sideBarBox.classList.add('openBar');
+        isOpen = true;
+    }
+}
+function closeSideBar() {
+    if (isOpen == true) {
+        sideBarBox.classList.remove('openBar');;
+        sideBarBox.classList.add('closeBar');
+        isOpen = false;
+    }
+}
